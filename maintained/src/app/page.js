9be084ffd1a8bf5ba0bf0e64cc840d400986cda8
KerryPower <kerrycpower@gmail.com>
2024-09-19 19:23:51 +1200

    removed old directory

--- maintained/src/app/page.js
+++ maintained/src/app/page.js
@@ -1,22 +1,22 @@
-import FaultCard from "@/components/Fault/FaultCardCondensed"
-const faultData = {
-  title: "test",
-  description: "test fault list",
-  serial_number: "test1234",
-  make: "test",
-  model: "case",
-  severity: "urgent",
-  image: "example.url"
-};
+import FaultCard from "@/components/Fault/FaultCardCondensed";
+import faultData from "../test-data/fault-data";
+import { Box } from "@mui/material";
 
-export default function page() {
+export default function Page() {
   return (
-  
-  <>
-    <p>hello</p>
-    <FaultCard fault={faultData} />
-  </>
-
-  )
-
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'space-around',
+          gap: 2,
+          padding: 2
+        }}>
+        {faultData.map((fault, index) => (
+          <FaultCard key={index} fault={fault} />
+        ))}
+      </Box>
+    </>
+  );
 }
